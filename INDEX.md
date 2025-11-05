05921dff9b2574beb48e4d4a3d292b8dc249461c
Marcelo Wiebbelling <mrodrigow@gmail.com>
2025-11-05 13:11:26 +0100

    Java 25 Spring Boot 3.5

--- INDEX.md
+++ INDEX.md
@@ -1,1 +1,2 @@
 # Semantic Releasing
+Java 25 with Spring Boot 3.5 release.
---
+++ src/main/java/com/github/marcelorodrigo/semantic_releasing/SemanticReleasingApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcelorodrigo.semantic_releasing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanticReleasingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanticReleasingApplication.class, args);
+	}
+
+}
