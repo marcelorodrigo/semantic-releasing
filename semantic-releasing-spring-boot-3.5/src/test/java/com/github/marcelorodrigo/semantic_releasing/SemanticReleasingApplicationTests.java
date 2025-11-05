2cad5314093ddb14199ec811cffc741e5f3f2511
Marcelo Wiebbelling <mrodrigow@gmail.com>
2025-11-05 18:23:42 +0100

    feat: initialize Spring Boot project with Maven configuration and application setup

---
+++ semantic-releasing-spring-boot-3.5/src/test/java/com/github/marcelorodrigo/semantic_releasing/SemanticReleasingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelorodrigo.semantic_releasing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanticReleasingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
